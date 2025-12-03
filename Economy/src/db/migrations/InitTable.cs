e0367dd8b9267f499ca9ec14782bac3291e1d8c4
K4ryuu <104531589+K4ryuu@users.noreply.github.com>
2025-12-03 20:45:43 +0100

    fix: Ensure EconomyPlayers table creation and deletion is conditional

--- Economy/src/db/migrations/InitTable.cs
+++ Economy/src/db/migrations/InitTable.cs
@@ -7,14 +7,18 @@ public class InitTable : Migration
 {
     public override void Up()
     {
-        Create.Table("EconomyPlayers")
-            .WithColumn("Id").AsInt32().PrimaryKey().Identity()
-            .WithColumn("SteamId64").AsInt64().NotNullable()
-            .WithColumn("Balance").AsFixedLengthString(8192).NotNullable().WithDefaultValue("{}");
+        if (!Schema.Table("EconomyPlayers").Exists())
+        {
+            Create.Table("EconomyPlayers")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("SteamId64").AsInt64().NotNullable()
+                .WithColumn("Balance").AsFixedLengthString(8192).NotNullable().WithDefaultValue("{}");
+        }
     }
 
     public override void Down()
     {
-        Delete.Table("EconomyPlayers");
+        if (Schema.Table("EconomyPlayers").Exists())
+            Delete.Table("EconomyPlayers");
     }
 }
